f45dd4b54709b415da140e4ae453e8ac5743e662
DeysiAnali <130004468+DeysiAnali@users.noreply.github.com>
2023-07-14 18:31:12 -0500

    Primer commit

---
+++ ProyectoTienda/src/main/java/Principal/ProyectoTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Principal;
+
+/**
+ *
+ * @author Deysi
+ */
+public class ProyectoTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
